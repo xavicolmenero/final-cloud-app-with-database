4935da2e2eb4244fb9a250aa6f0c1a8e4bb42c3b
Xavi Colmenero <xavicolmenero@gmail.com>
2021-11-18 10:59:36 +0100

    admin v2

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -28,11 +28,26 @@ class LessonAdmin(admin.ModelAdmin):
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'question_grade')
+    list_display = ('question_text', 'question_grade', 'get_lesson')
+    list_filter = ['question_grade']
+    search_fields = ['question_text']
+    def get_lesson(self, obj):
+        return obj.lesson.title
+    get_lesson.short_description = 'Lesson'
+    get_lesson.admin_order_field = 'order'   
  
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('get_question', 'choice_content', 'is_correct')
+    list_filter = ['is_correct']
+    search_fields = ['choice_content']
+    def get_question(self, obj):
+        return obj.question.question_text
+    get_question.short_description = 'Question'
+    get_question.admin_order_field = 'question_text'
+
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
